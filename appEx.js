5b353d28d849b9b443b13d55204246dfb1d5b811
elvis <cenelvis@gmail.com>
2022-03-12 14:46:23 -0500

    Handling request notes

--- appEx.js
+++ appEx.js
@@ -4,36 +4,55 @@ const express = require('express');
 const app = express();
 // We store/assign the functions to 'app'
 
-// Middleware
-// => function that can modify incoming request data
+// Middle wear
 app.use(express.json());
+// => function that can modify incoming request data
+
+/*_________________________________________________________________
+// #1
+app.get('/', (req, res) => {
+  // '/' => main URL
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...');
+});
+__________________________________________________________________*/
 
 // #3 ______________________________________________________________
 
-// File was a json file
-// JSON.parse => to convert into JS
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+// File was a json file
+// JSON.parse => to convert into JS
 
-const getAllTours = (req, res) => {
+// Route handler
+// => what to do when someone hit '/api/v1/tours'
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: {
       tours,
+      // => 'tours' from top
     },
   });
-};
+});
 
 // #5 ________________________________________________________________
 
-const getTour = (req, res) => {
+// :id => is a parameter, we create them using ":"
+// ? => optional, if id is not enter, it will not throw err
+app.get('/api/v1/tours/:id?', (req, res) => {
   // req.params => all parameters enter on the URL
   console.log(req.params);
 
   // Get ID param in URL & convert to number
-  const id = req.params.id * 1;
+  const id = +req.params.id;
 
   // Get tour based on ID enter
   const tour = tours.find((el) => el.id === id);
@@ -49,11 +68,11 @@ const getTour = (req, res) => {
       tour,
     },
   });
-};
+});
 
 // #4 ________________________________________________________________
 
-const createTour = (req, res) => {
+app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
 
   // Creating new tours/ID
@@ -81,12 +100,12 @@ const createTour = (req, res) => {
       });
     }
   );
-};
+});
 
 // #6 ________________________________________________________________
 // Updating existing tours
 
-const updateTour = (req, res) => {
+app.patch('/api/v1/tours/:id', (req, res) => {
   if (+req.params.id > tours.length) {
     return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
   }
@@ -94,32 +113,18 @@ const updateTour = (req, res) => {
   res
     .status(200)
     .json({ status: 'success', data: { tour: '<Updated tour Here...>' } });
-};
+});
 
 // #7 ________________________________________________________________
 // Deleting Tours
 
-const deleteTour = (req, res) => {
+app.delete('/api/v1/tours/:id', (req, res) => {
   if (+req.params.id > tours.length) {
     return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
   }
 
   res.status(204).json({ status: 'success', data: null });
-};
-// __________________________________________________________________
-
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-
-// app.get('/api/v1/tours/:id?', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+});
 
 const port = 3000;
 app.listen(port, () => {
--- section-6-notes.js
+++ section-6-notes.js
@@ -74,3 +74,56 @@
 
 // -------------------------------------------- Object.assign({Obj1}, {Obj2})
 // => Merge 2 objects and returns a new Object
+
+/////////////////////////////////////////////////////////////////////
+
+// #5
+// Responding to URL Parameters
+
+// ----------------------------------------- ":"(parameter) "?"(optional)
+// => we can set variables to the URL with ":"
+// Ex => '/api/v1/tours/:id/:pg/:x?' => (id,pg,x) = parameters we can manipulate/consume
+// ? => 'x?' => optional parameter, it will not throw an error if no value is enter in URL for 'X'
+
+/////////////////////////////////////////////////////////////////////
+
+// #6
+// Handling Patch Requests
+
+// ----------------------------------------- app.patch("URL/:ID", (req,res) {})
+// Exampple on => "appEx.js"
+
+/////////////////////////////////////////////////////////////////////
+
+// #7
+// Handling DELETE Request
+
+// ----------------------------------------- app.delete("URL/:ID", (req,res) {})
+// Same as .patch(), But the difference is
+// => res.status(204) = no content
+// => .json( data: null )
+
+/////////////////////////////////////////////////////////////////////
+
+// #8
+// Refactoring Our Routes
+
+// appEx.js => was created to keep our examples from previous lectures #(1~7)
+
+// ------------------------------------------- app.route('URL')
+// => to target a URL, basically saying "this URL"
+// => we can chain methods
+// Ex => app.route('URL').get().route().post().patch().delete()
+
+/////////////////////////////////////////////////////////////////////
+
+// #9
+// Middleware and the Request-Response Cycle
+
+// In Express => everything is middleware (even routers)
+// => middleware are functions that run in the middle when we send/receive request
+
+// Middleware stack => all middleware function run in sync mode, one after another in order
+
+// Reques-Response Cycle => Everything, All the process together in the Middleware
+// From => Incoming request => Middleware stack/functions => response
