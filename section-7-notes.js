530c6f3639bcdbf41d01d070585ab08372879f97
elvis <cenelvis@gmail.com>
2022-03-19 15:50:42 -0400

    mongosh

---
+++ section-7-notes.js
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////////////
+
+// #1
+// Connecting to out hosted Database
+
+// 1. Connect to cluster => get Connect link in mongoDB
+// 2. Type link in Command Promt
+
+// ---------------------------------------------- use natours
+// Type in command promt => to use natours
+
+// ---------------------------------------------- db.tours.find()
+// return our tours created
