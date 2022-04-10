b5a2fb551a6c219711d199302924d8f292ca6cc3
elvis <cenelvis@gmail.com>
2022-04-10 15:38:13 -0400

    Signing up Users, Jason Web Token, npm i jasonwebtoken, jwt.sign()

--- section-10-notes.js
+++ section-10-notes.js
@@ -18,6 +18,8 @@
 // #3
 // Managing Passwords
 
+// Encrypting passwords
+
 // "passwordConfirm" changes in "userModel"
 
 // ------------------------------------ validator : function() {}
@@ -47,3 +49,36 @@
 // required: true; => in the schema, means is only a required imput
 // not required to be persisted in the database
 // thats why we can set => "passwordConfirm" = undefined
+
+//////////////////////////////////////////////////////////////////////////
+
+// #4
+// Jason Web Token (JWT)
+
+// => is like an encrypt password assigned to a matching user && password
+// => which the server receives and verify
+// => if valid => it will send protected data to the client
+
+//////////////////////////////////////////////////////////////////////////
+
+// #5
+// Signing up Users
+
+// Changes made on "authController.js"
+// changes on .signup => To specify the only things we need to create a newUser
+// => so no one can manually imput a new role to the new created account
+// => no longer possible to add => admin, instead, we edit it from compass or mongoDB
+
+// https://github.com/auth0/node-jsonwebtoken
+// ------------------------------------------ npm i jasonwebtoken
+// => package for JWT
+
+// ------------------------------------------ jwt.sign(payload, secretOrPrivateKey, {options, callback})
+// payload => what to convert
+// secretOrPrivateKey => self created secret key (must be at least 32 long)
+
+// ------------------------------------------ jwt.verify(token, secretOrPrivateKey, [options, callback])
+//
+
+// *********** IMPROTANT ************
+// SECRET code should be at least 32 characters long
