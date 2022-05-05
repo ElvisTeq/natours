2db7099f1450b7bb14700adbd14faf8f6de5d14d
elvis <cenelvis@gmail.com>
2022-05-05 13:13:10 -0400

    Creating Bate Template, More examples of writting HTML in PUG

--- section-12-notes.js
+++ section-12-notes.js
@@ -36,6 +36,7 @@
 
 // ******************** Buffer Code
 // => We can write JS in "base.pug"
+// Ex => h2= user.toUpperCase()
 
 // ******************** Unbuffer Code
 // --------------------------------------- (- const x = 9)
@@ -44,7 +45,38 @@
 // ******************** Interpolation Code "#{}"
 // => Added template string with "#{}" instead of "${}" from ES6
 // => Adding Code to a String
+// Ex => title Natours | #{tour}
 
 // ******************* Important ***********************
 // => We use "app.use(express.static(path.join(__dirname, 'public')));" in "app.js"
 // => This causes that "images" and "css/style.css" will automatically be search in "public" folder in "base.pug"
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+// #3
+// Creating Our Base Template
+
+// ----------------------------------- header.header
+// => <header class="header"> in "pug"
+// "." => points to a class
+
+// ----------------------------------- nav.nav.nav--tours
+// => <nav class="nav nav--tours">
+
+// ----------------------------------- a.nav__el(href='#') All tours
+// => <a href="#" class="nav__el">All tours</a>
+
+// ----------------------------------- .header__logo
+// => <div class="header__logo">
+// div => "." to automatically create a div element
+
+// ----------------------------------- (src='' alt='' href='')
+// () => use to add attributes
+// Ex => <img src="img/logo-white.png"/>
+// Ex => img(src='img/logo-white.png')
+
+// ----------------------------------- li: a(href='#') About us
+// => <li><a href="#">About us</a></li>
+
+// ----------------------------------- p.footer__copyright &copy; by Elvis
+// => <p class="footer__copyright">&copy; Elvis</p>
